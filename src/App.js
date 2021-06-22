d2f2fbb30aaf56cb915ba916f50ef0014031f72a
Dan R Beste <drb@wishalloy.com>
2021-06-21 20:49:34 -0500

    Feat: Pagination

--- src/App.js
+++ src/App.js
@@ -5,19 +5,28 @@ import Brewery from "./Brewery";
 
 function App() {
   const [breweries, setBreweries] = useState([]);
+  const [initialState, setInitialState] = useState(true);
+  const [page, setPage] = useState(1);
 
-  const filter = (element) => {
-    const search = element.target.value;
+  const filter = () => {
+    const search = document.getElementById("searchBar").value;
 
     fetch(
       "https://api.openbrewerydb.org/breweries/search?query=" +
-        encodeURI(search)
+        encodeURI(search) +
+        "&page=" +
+        page +
+        "&per_page=20"
     )
       .then((response) => {
         return response.json();
       })
       .then((json) => {
+        setInitialState(false);
         setBreweries(json);
+      })
+      .catch((error) => {
+        alert("Error: " + error.message);
       });
   };
 
@@ -29,11 +38,20 @@ function App() {
       })
       .then((json) => {
         setBreweries(json);
+      })
+      .catch((error) => {
+        alert("Error: " + error.message);
       });
   }, []);
+  useEffect(() => {
+    if (initialState === false) {
+      filter();
+    }
+  }, [page]);
   return (
     <div className="App">
       <input
+        id="searchBar"
         type="search"
         defaultValue="🔍 Search Breweries"
         onChange={filter}
@@ -48,7 +66,33 @@ function App() {
             : null
         }
       />
-      <p>Results: {breweries.length}</p>
+      <br />
+      {!initialState ? (
+        <div className="pagination">
+          <button
+            disabled={initialState}
+            onClick={() => {
+              setPage(page !== 1 ? page - 1 : page);
+            }}
+          >
+            Previous Page
+          </button>
+          <p>Page: {page}</p>
+          <button
+            disabled={initialState}
+            onClick={() => {
+              setPage(breweries.length > 0 ? page + 1 : page);
+            }}
+          >
+            Next Page
+          </button>
+        </div>
+      ) : null}
+      {breweries.length ? (
+        <p>{breweries.length} results</p>
+      ) : (
+        <p>No results!</p>
+      )}
       {breweries.map((brewery) => {
         return <Brewery key={brewery.id} brewery={brewery} />;
       })}
