41682389e8b46a3f48cf221fb34db62f9cabb545
Dan R Beste <drb@wishalloy.com>
2021-06-22 00:23:56 -0500

    Chore: Code cleanup
     * Wrap relevant functions in useCallback to prevent them from being
       called when the state they interact with has not changed
     * Moved "magic" logic I had tucked away in my JSX into functions

--- src/App.js
+++ src/App.js
@@ -1,60 +1,131 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import "./App.css";
 import "./Brewery.css";
 import Brewery from "./Brewery";
 
 function App() {
   const [breweries, setBreweries] = useState([]);
-  const [initialState, setInitialState] = useState(true);
+  const [input, setInput] = useState("🔍 Search Breweries");
+  const [pristine, setPristine] = useState(true);
   const [page, setPage] = useState(1);
+  const [results, setResults] = useState(0);
+  const perPage = 20;
 
-  const filter = () => {
-    const search = document.getElementById("searchBar").value;
+  const isDefaultInput = useCallback(() => {
+    return input === "🔍 Search Breweries";
+  }, [input]);
 
+  function previousPage() {
+    if (page === 1) {
+      return page;
+    }
+
+    return page - 1;
+  }
+
+  function nextPage() {
+    if (breweries.length < 0) {
+      return page;
+    }
+
+    return page + 1;
+  }
+  function isFirstPage() {
+    return page === 1;
+  }
+
+  function isLastPage() {
+    return page * perPage >= results;
+  }
+
+  function pageLowestResult() {
+    return page * perPage - perPage + 1;
+  }
+
+  function pageHighestResult() {
+    if (page * perPage < results) {
+      return page * perPage;
+    }
+
+    return results;
+  }
+
+  const totalResults = useCallback(() => {
     fetch(
-      "https://api.openbrewerydb.org/breweries/search?query=" +
-        encodeURI(search) +
-        "&page=" +
-        page +
-        "&per_page=20"
+      "https://api.openbrewerydb.org/breweries/search?query=" + encodeURI(input)
     )
       .then((response) => {
         return response.json();
       })
       .then((json) => {
-        setInitialState(false);
+        setResults(json.length);
+      })
+      .catch((error) => {
+        alert("Error: " + error.message);
+      });
+  }, [input]);
+
+  const search = useCallback(() => {
+    // Paginated results:
+    fetch(
+      "https://api.openbrewerydb.org/breweries/search?query=" +
+        encodeURI(input) +
+        "&page=" +
+        page +
+        "&per_page=" +
+        perPage
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((json) => {
+        setPristine(false);
         setBreweries(json);
       })
       .catch((error) => {
         alert("Error: " + error.message);
       });
-  };
+  }, [page, input]);
 
-  // Default state:
   useEffect(() => {
+    // Set the default state of the application (a default list of OBDB breweries):
     fetch("https://api.openbrewerydb.org/breweries")
       .then((response) => {
         return response.json();
       })
       .then((json) => {
         setBreweries(json);
+        setResults(json.length);
       })
       .catch((error) => {
         alert("Error: " + error.message);
       });
   }, []);
+
+  // Fetch new brewery results when a user requests a new page:
   useEffect(() => {
-    if (initialState === false) {
-      filter();
+    if (!isDefaultInput()) {
+      search();
     }
-  }, [page]);
+  }, [page, search, input, isDefaultInput, pristine]);
+
+  // Only update the total number of results if the input has changed:
+  useEffect(() => {
+    if (!isDefaultInput()) {
+      setPage(1);
+      totalResults();
+    }
+  }, [input, isDefaultInput, totalResults]);
+
   return (
     <div className="App">
       <input
         id="searchBar"
         type="search"
         defaultValue="🔍 Search Breweries"
-        onChange={filter}
+        onChange={(e) => {
+          setInput(e.target.value);
+        }}
         onFocus={(e) =>
           e.target.value === "🔍 Search Breweries"
             ? (e.target.value = "")
@@ -67,29 +138,32 @@ function App() {
         }
       />
       <br />
-      {!initialState ? (
+      {!pristine ? (
         <div className="pagination">
           <button
-            disabled={initialState}
+            disabled={pristine || isFirstPage()}
             onClick={() => {
-              setPage(page !== 1 ? page - 1 : page);
+              setPage(previousPage());
             }}
           >
             Previous Page
           </button>
           <p>Page: {page}</p>
           <button
-            disabled={initialState}
+            disabled={pristine || isLastPage()}
             onClick={() => {
-              setPage(breweries.length > 0 ? page + 1 : page);
+              setPage(nextPage());
             }}
           >
             Next Page
           </button>
         </div>
       ) : null}
-      {breweries.length ? (
-        <p>{breweries.length} results</p>
+      {pristine ? null : breweries.length ? (
+        <p>
+          Displaying results {pageLowestResult()}-{pageHighestResult()} of{" "}
+          {results}
+        </p>
       ) : (
         <p>No results!</p>
       )}
